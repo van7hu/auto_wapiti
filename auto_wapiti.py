633c4e87ae75cadd89becd7af9583e14cffcf54a
van7hu <van7hu@gmail.com>
2018-08-23 08:23:46 +0700

    fix bugs

--- auto_wapiti.py
+++ auto_wapiti.py
@@ -2,7 +2,6 @@ import subprocess, os, sys
 from time import sleep
 
 # 10 jobs is recommended for 4G of RAM
-#subprocess.check_output('ps aux | grep "\-\-output\-verbose" | wc -l', stdout=subprocess.PIPE)
 
 def get_job_count():
     me = subprocess.check_output(['ps aux | grep wapiti | wc -l'], shell=True)
@@ -22,9 +21,9 @@ for line in lines:
 for url in urls:
     while True:
         if get_job_count() < int(sys.argv[2]):
-            command = './wapiti --color -m backup,blindsql,buster,crlf,delay,exec,file,htaccess,methods,nikto,permanentxss,shellshock,sql,ssrf,xss -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36" -v 2 -u ' + url + " &"
+            command = './wapiti --color -m backup,blindsql,buster,crlf,delay,exec,file,htaccess,methods,nikto,permanentxss,shellshock,sql,ssrf,xss -A "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36" -v 2 -u ' + url
             print "Executing command: " + command
-            #os.system(command)
+            os.system(command)
             sleep(2)
             break
         else:
